22a2998910d767fda2422f6410d02319a65362a7
hjk7902 <hjk7902@gmail.com>
2023-09-06 11:23:23 +0900

    자바 스프링 프레임워크와 스프링 부트 - 원리부터 실전까지

--- SpringBook_Chapter3_JDBC/src/main/java/com/example/myapp/jdbc/EmpVO.java
+++ SpringBook_Chapter3_JDBC/src/main/java/com/example/myapp/jdbc/EmpVO.java
@@ -2,6 +2,12 @@ package com.example.myapp.jdbc;
 
 import java.sql.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter @Getter
+@ToString
 public class EmpVO {
 
 	private int employeeId;
@@ -15,80 +21,4 @@ public class EmpVO {
 	private double commissionPct;
 	private int managerId;
 	private int departmentId;
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public Date getHireDate() {
-		return hireDate;
-	}
-	public void setHireDate(Date hireDate) {
-		this.hireDate = hireDate;
-	}
-	public String getJobId() {
-		return jobId;
-	}
-	public void setJobId(String jobId) {
-		this.jobId = jobId;
-	}
-	public double getSalary() {
-		return salary;
-	}
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-	public double getCommissionPct() {
-		return commissionPct;
-	}
-	public void setCommissionPct(double commissionPct) {
-		this.commissionPct = commissionPct;
-	}
-	public int getManagerId() {
-		return managerId;
-	}
-	public void setManagerId(int managerId) {
-		this.managerId = managerId;
-	}
-	public int getDepartmentId() {
-		return departmentId;
-	}
-	public void setDepartmentId(int departmentId) {
-		this.departmentId = departmentId;
-	}
-	
-	@Override
-	public String toString() {
-		return "EmpVO [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", email="
-				+ email + ", phoneNumber=" + phoneNumber + ", hireDate=" + hireDate + ", jobId=" + jobId + ", salary="
-				+ salary + ", commissionPct=" + commissionPct + ", managerId=" + managerId + ", departmentId="
-				+ departmentId + "]";
-	}
-	
-	
 }
