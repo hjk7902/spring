26fbcfee280a401861e7db501f4c07413b58d8c2
허진경 <hjk7902@gmail.com>
2023-12-17 18:58:27 +0900

    JWT 생성기

--- SpringBook_Sub_Security(JWT)/src/main/java/com/example/myapp/jwt/JwtTokenProvider.java
+++ SpringBook_Sub_Security(JWT)/src/main/java/com/example/myapp/jwt/JwtTokenProvider.java
@@ -1,7 +1,9 @@
 package com.example.myapp.jwt;
 
-import java.security.Key;
 import java.util.Date;
+import java.util.UUID;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,30 +16,53 @@ import com.example.myapp.member.model.Member;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.MacAlgorithm;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * JWT를 생성하고 검증하는 서비스를 제공
+ * @author JinKyoung Heo
+ * @version 1.0
+ */
 @Slf4j
 public class JwtTokenProvider {
 
-    private static String secretKeyStr = "qwertyuiop1234567890asdfghjklqwertyuiop";
-    private static Key key;
+	/**
+	 * 키에 사용할 secret값
+	 */
+	String id = UUID.randomUUID().toString();
 
-    private long tokenValidTime = 30 * 60 * 1000L; // 토큰 유효시간 30분
+	/**
+	 * 알고리즘
+	 */
+	private static MacAlgorithm alg = Jwts.SIG.HS256;
+    
+	/**
+	 * 개인키
+	 */
+	private static SecretKey key = alg.key().build();
+    
+	/**
+	 * 토큰 유효기간
+	 */
+    private long tokenValidTime = 30 * 60 * 1000L;
     
     @Autowired
     UserDetailsService userDetailsService;
 
     @PostConstruct
     protected void init() {
-    	key = Keys.hmacShaKeyFor(secretKeyStr.getBytes());
+    	log.debug("token generated : " + id);
     }
 
-    // JWT 토큰 생성
-    public String createToken(Member member) {
-    	log.info("token created");
+    /**
+     * 토큰을 만들어 반환
+     * @param member 사용자 정보를 저장한 객체, 클래임에 사용자 정보를 저장하기 위해 필요
+     * @return 생성된 토큰
+     */
+    public String generateToken(Member member) {
         Claims claims = Jwts.claims()
         		.subject(member.getUserid())
         		.issuer(member.getName())
@@ -48,40 +73,56 @@ public class JwtTokenProvider {
                 .claims(claims) // 정보 저장
                 .issuedAt(now) // 토큰 발행 시간 정보
                 .expiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
-                .signWith(key)  // 사용할 암호화 알고리즘과 signature 에 들어갈 secret값 세팅
+                .signWith(key, alg)  // 사용할 암호화 알고리즘과 signature 에 들어갈 secret값 세팅
                 .compact();
     }
 
-    // JWT 토큰에서 인증 정보 조회
+    /**
+     * JWT 토큰에서 인증 정보 조회
+     * @param token 토큰
+     * @return 인증정보 Authentication 객체
+     */
     public Authentication getAuthentication(String token) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserId(token));
         log.info(userDetails.getUsername());
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    // 토큰에서 회원 정보 추출
+    /**
+     * 토큰에서 회원 정보 추출
+     * @param token 토큰
+     * @return 토큰에서 사용자 아이디를 추출해서 반환
+     */
     public String getUserId(String token) {
     	log.info(token);
         return Jwts.parser()
-        		.setSigningKey(key)
+        		.verifyWith(key)
         		.build()
         		.parseSignedClaims(token)
         		.getPayload()
         		.getSubject();
     }
 
-    // Request의 Header에서 token 값을 가져옵니다. "X-AUTH-TOKEN" : "TOKEN값'
+    /**
+     * Request의 Header에서 token 값을 가져옴 "X-AUTH-TOKEN" : "TOKEN값'
+     * @param request 요청객체
+     * @return 토큰
+     */
     public String resolveToken(HttpServletRequest request) {
         return request.getHeader("X-AUTH-TOKEN");
     }
 
-    // 토큰의 유효성 + 만료일자 확인
-    public boolean validateToken(String jwtToken) {
+    /**
+     * 토큰의 유효성과 만료일자 확인
+     * @param token 토큰
+     * @return 토큰이 유효한기 확인, 유효하면 true 반환
+     */
+    public boolean validateToken(String token) {
         try {
             Jws<Claims> claims = Jwts.parser()
-            		.setSigningKey(key)
+            		.verifyWith(key)
             		.build()
-            		.parseSignedClaims(jwtToken);
+            		.parseSignedClaims(token);
             return !claims.getPayload().getExpiration().before(new Date());
         } catch (Exception e) {
             return false;
--- SpringBook_Sub_Security(JWT)/src/main/java/com/example/myapp/member/controller/MemberRestController.java
+++ SpringBook_Sub_Security(JWT)/src/main/java/com/example/myapp/member/controller/MemberRestController.java
@@ -3,7 +3,9 @@ package com.example.myapp.member.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import com.example.myapp.jwt.JwtTokenProvider;
 import com.example.myapp.member.model.Member;
 import com.example.myapp.member.service.IMemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -37,7 +40,19 @@ public class MemberRestController {
         if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
-        return jwtTokenProvider.createToken(member);
+        return jwtTokenProvider.generateToken(member);
     }
-    
+
+    @GetMapping("/test_jwt")
+    public String testJwt(HttpServletRequest request) {
+    	String token = jwtTokenProvider.resolveToken(request);
+    	log.info("token {}", token);
+    	Authentication auth = jwtTokenProvider.getAuthentication(token);
+    	log.info("principal {}, name {}, authorities {}", 
+    			auth.getPrincipal(), auth.getName(), auth.getAuthorities());
+    	log.info("isValid {}", jwtTokenProvider.validateToken(token));
+    	return jwtTokenProvider.getUserId(token);
+    }
+   
 }
+
