5fd5e342becb4d079b63878ac36912888c1c5689
나자바바 <hjk7902@gmail.com>
2025-01-01 00:14:38 +0900

    자바 스프링 프레임워크와 스프링 부트 - 원리부터 실전까지
    자바 스프링 프레임워크와 스프링 부트 - 원리부터 실전까지

--- SpringBook_Chapter4_WebMVC/src/main/java/com/example/myapp/hr/controller/EmpController.java
+++ SpringBook_Chapter4_WebMVC/src/main/java/com/example/myapp/hr/controller/EmpController.java
@@ -1,22 +1,25 @@
 package com.example.myapp.hr.controller;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.myapp.hr.model.EmpVO;
+import com.example.myapp.hr.EmpNotFoundException;
+import com.example.myapp.hr.model.Emp;
 import com.example.myapp.hr.service.IEmpService;
 
 @Controller
@@ -24,52 +27,56 @@ public class EmpController {
 
 	@Autowired
 	IEmpService empService;
-	
-	@RequestMapping(value="/hr/count")
+
+	@GetMapping({"/hr/count", "/hr/cnt"})
 	public String empCount(@RequestParam(value="deptid", required=false, defaultValue="0") int deptid, Model model) {
 		if(deptid==0) {
-			model.addAttribute("count", empService.getEmpCount());
+			model.addAttribute("count", empService.getEmpCount()); // 모든 사원의 수
 		}else {
-			model.addAttribute("count", empService.getEmpCount(deptid));
+			model.addAttribute("count", empService.getEmpCount(deptid)); // 부서 사원의 수
 		}
-		return "hr/count";
+		return "hr/count"; // WEB-INF/views/hr/count.jsp
 	}
 
-	@RequestMapping(value="/hr/list")
+	@GetMapping(value="/hr/list")
 	public String getAllEmps(Model model) {
-		List<EmpVO> empList = empService.getEmpList();
+		List<Emp> empList = empService.getEmpList();
 		model.addAttribute("empList", empList);
 		return "hr/list";
 	}
 
-	@RequestMapping(value="/hr/{employeeId}")
+	@GetMapping("/hr/{employeeId:[0-9]+}")
 	public String getEmpInfo(@PathVariable int employeeId, Model model) {
-		EmpVO emp = empService.getEmpInfo(employeeId);
-		model.addAttribute("emp", emp);
+		try {
+			Emp emp = empService.getEmpInfo(employeeId);
+			model.addAttribute("emp", emp);
+		}catch(EmptyResultDataAccessException e) {
+			throw new EmpNotFoundException("사원이 없습니다.");
+		}
 		return "hr/view";
 	}
 
-	@RequestMapping(value="/hr/insert", method=RequestMethod.GET)
+	@GetMapping("/hr/insert")
 	public String insertEmp(Model model) {
 		model.addAttribute("deptList", empService.getAllDeptId());
 		model.addAttribute("jobList", empService.getAllJobId());
 		model.addAttribute("managerList", empService.getAllManagerId());
 		return "hr/insertform";
 	}
-	
-	@RequestMapping(value="/hr/insert", method=RequestMethod.POST)
-	public String insertEmp(EmpVO emp, RedirectAttributes redirectAttributes) {
+
+	@PostMapping(value="/hr/insert")
+	public String insertEmp(Emp emp, RedirectAttributes redirectAttrs) {
 		try {
 			empService.insertEmp(emp);
-			redirectAttributes.addFlashAttribute("message", 
-					emp.getEmployeeId()+"번 사원정보가 입력되었습니다.");
+			redirectAttrs.addFlashAttribute("message", 
+					emp.getEmployeeId() + "번 사원정보가 입력되었습니다.");
 		}catch(RuntimeException e) {
-			redirectAttributes.addFlashAttribute("message", e.getMessage());
+			redirectAttrs.addFlashAttribute("message", e.getMessage());
 		}
 		return "redirect:/hr/list";
 	}
-	
-	@RequestMapping(value="/hr/update", method=RequestMethod.GET)
+
+	@GetMapping(value="/hr/update")
 	public String updateEmp(int empid, Model model) {
 		model.addAttribute("emp", empService.getEmpInfo(empid));
 		model.addAttribute("deptList", empService.getAllDeptId());
@@ -78,8 +85,8 @@ public class EmpController {
 		return "hr/updateform";
 	}
 	
-	@RequestMapping(value="/hr/update", method=RequestMethod.POST)
-	public String updateEmp(EmpVO emp, RedirectAttributes redirectAttributes) {
+	@PostMapping(value="/hr/update")
+	public String updateEmp(Emp emp, RedirectAttributes redirectAttributes) {
 		try {
 			empService.updateEmp(emp);
 			redirectAttributes.addFlashAttribute("message",
@@ -89,49 +96,55 @@ public class EmpController {
 		}
 		return "redirect:/hr/" + emp.getEmployeeId();
 	}
-	
-	@RequestMapping(value="/hr/delete", method=RequestMethod.GET)
+
+	@GetMapping(value="/hr/delete")
 	public String deleteEmp(int empid, Model model) {
 		model.addAttribute("emp", empService.getEmpInfo(empid));
 		return "hr/deleteform";
 	}
-
-	@RequestMapping(value="/hr/delete", method=RequestMethod.POST)
-	public String deleteEmp(int empid, String email, Model model, 
-			RedirectAttributes redirectAttrs) {
+ 
+	@PostMapping(value="/hr/delete")
+	public String deleteEmp(int empid, String email, RedirectAttributes redAttr, Model model) {
 		try {
 			int deletedRow = empService.deleteEmp(empid, email);
-			if(deletedRow>0) {
-				redirectAttrs.addFlashAttribute("message",
-						empid + "번 사원정보가 삭제되었습니다.");
+			if(deletedRow > 0) {
+				redAttr.addFlashAttribute("message", empid + "번 사원정보가 삭제되었습니다.");
 				return "redirect:/hr/list";
 			}else {
-				model.addAttribute("message", "사번과 이메일 주소가 다릅니다.");
+				model.addAttribute("message", "ID 또는 Email이 다릅니다.");
 				model.addAttribute("emp", empService.getEmpInfo(empid));
 				return "hr/deleteform";
 			}					
 		}catch(RuntimeException e) {
-			redirectAttrs.addFlashAttribute("message", e.getMessage());
-			return "redirect:/hr/list";
+			model.addAttribute("message", e.getMessage());
+			model.addAttribute("emp", empService.getEmpInfo(empid));
+			return "hr/deleteform";
 		}
 	}
 	
-	@ExceptionHandler({SQLException.class, RuntimeException.class})
-	public String runtimeException(HttpServletRequest request, Exception ex,
-			Model model) {
-		model.addAttribute("url", request.getRequestURI());
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@ExceptionHandler({RuntimeException.class, EmptyResultDataAccessException.class})
+	public String runtimeException(HttpServletRequest request, Exception ex, Model model) {
+		logger.error("Emp Error: URL-{}, EX-{} ", request.getRequestURL(), ex);
 		model.addAttribute("exception", ex);
+		model.addAttribute("url", request.getRequestURL());
 		return "error/runtime";
 	}
 	
-	
-	@RequestMapping(value="/hr/json")
-	public @ResponseBody List<EmpVO> getEmpJSONList() {
-		return empService.getEmpList();
+	@ResponseBody 
+	@GetMapping(value="/hr/json")
+	public List<Emp> getEmpJSONList() {
+		List<Emp> empList = empService.getEmpList();
+		return empList;
 	}
 	
-	@RequestMapping(value="/hr/json/{employeeId}")
-	public @ResponseBody EmpVO getEmpJSONInfo(@PathVariable int employeeId) {
-		return empService.getEmpInfo(employeeId);
+	@GetMapping(value="/hr/json/{employeeId}")
+	public @ResponseBody Emp getEmpJSONInfo(@PathVariable int employeeId) {
+		Emp emp = empService.getEmpInfo(employeeId);
+		return emp;
 	}
-}//end class
+	
+
+}
+
